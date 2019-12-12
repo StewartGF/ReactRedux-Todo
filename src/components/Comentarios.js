8bbf6954439c8d3c968d66572054adbb59191607
Stewart Granger Flores <stewart.grngrf@gmail.com>
2019-12-11 22:15:50 -0300

    listado de comentarios funcionando

--- src/components/Comentarios.js
+++ src/components/Comentarios.js
@@ -5,22 +5,25 @@ function Comentarios(todos) {
   const todo = useSelector(state => state.todos);
   const todoSeleccionado = useSelector(state => state.seleccionado);
   let comentarios;
-  let parrafo;
+  let con;
   if (todo) {
     comentarios = todo
       .filter(todo => todo.id === parseInt(todoSeleccionado.id))
       .map(x => x.mensajes);
-    parrafo = comentarios.map(x => x.comentario);
-    console.log(parrafo);
+    con = []
+      .concat(...comentarios)
+      .map(x => ({ id: x.comentarioID, comentario: x.comentario }));
+    console.log(con);
   }
   // traer todos los todo y después mapear para encontrar el del ID
   return (
     <div className="container-comentarios">
       <h1>Comentarios</h1>
-      {comentarios.map(comentario => (
-        <div className="comentario-item">
+      {con.map(comentario => (
+        <div key={comentario.id} className="comentario-item">
           <div className="descripcion">
-            <p className="texto"> {comentario.comentario} </p>
+            <p className="id"> {comentario.id}</p>
+            <p className="texto">{comentario.comentario} </p>
           </div>
         </div>
       ))}

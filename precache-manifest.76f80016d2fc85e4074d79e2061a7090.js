6c93a3be756659ab17b3557ce8c3183a74c1827c
Stewart Granger Flores <stewart.grngrf@gmail.com>
2019-12-15 19:25:21 -0300

    Updates

---
+++ precache-manifest.76f80016d2fc85e4074d79e2061a7090.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "292e57ba9dc57e9159e5a6530e9364e6",
+    "url": "/ReactRedux-Todo/index.html"
+  },
+  {
+    "revision": "be7e9ada449b7fa72208",
+    "url": "/ReactRedux-Todo/static/css/2.dfe7030c.chunk.css"
+  },
+  {
+    "revision": "5b9c7aadf1a9621355db",
+    "url": "/ReactRedux-Todo/static/css/main.3dceff13.chunk.css"
+  },
+  {
+    "revision": "be7e9ada449b7fa72208",
+    "url": "/ReactRedux-Todo/static/js/2.70122e30.chunk.js"
+  },
+  {
+    "revision": "5b9c7aadf1a9621355db",
+    "url": "/ReactRedux-Todo/static/js/main.ee309bde.chunk.js"
+  },
+  {
+    "revision": "f69d1fc119661bb6dff8",
+    "url": "/ReactRedux-Todo/static/js/runtime~main.4b79ba3b.js"
+  }
+]);
